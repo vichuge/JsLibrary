49bcff55eef0a945872ffc47cdcbdde5ac688d10
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-24 14:58:29 -0600

    Quit jquery to use JS only

--- src/js/script.js
+++ src/js/script.js
@@ -8,31 +8,26 @@ function Book(name, author, numpages, read) {
 }
 
 function addBookToLibrary() {
-  const name = $('#name').val();
-  const author = $('#author').val();
-  const numpages = $('#numPages').val();
+  const name = document.getElementById("name").value;
+  const author = document.getElementById("author").value;
+  const numpages = document.getElementById("numPages").value;
   const book = new Book(name, author, numpages, false);
   myLibrary.push(book);
   listBooks();
   emptyInputs();
-  console.log(myLibrary);
 }
 
 function emptyInputs() {
-  $('#name').val('');
-  $('#author').val('');
-  $('#numPages').val('');
+  document.getElementById("name").value = '';
+  document.getElementById("author").value = '';
+  document.getElementById("numPages").value = '';
 }
 
 function listBooks() {
-  $('#list').empty();
+  document.getElementById("list").innerHTML = '';
+  d = document.getElementById("list");
   for (let i = 0; i < myLibrary.length; i++) {
-    $('#list').append(
-      d = $('<div>').prop({
-        className: 'col-4'
-      }),
-      d.append('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + myLibrary[i].name + ' (' + myLibrary[i].numpages + ' pages)</h5><h6 class="card-subtitle mb-2 text-muted">' + myLibrary[i].author + '</h6><p class="card-text">Have you read it before? ' + myLibrary[i].read + '</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook(' + i + ')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook(' + i + ')" class="card-link">Change read</button></div></div></div>'),
-    );
+    d.innerHTML += '<div class="col-5"><div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + myLibrary[i].name + ' (' + myLibrary[i].numpages + ' pages)</h5><h6 class="card-subtitle mb-2 text-muted">' + myLibrary[i].author + '</h6><p class="card-text">Have you read it before? ' + myLibrary[i].read + '</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook(' + i + ')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook(' + i + ')" class="card-link">Change read</button></div></div></div></div>';
   }
 }
 
@@ -45,6 +40,3 @@ function readBook(id) {
   myLibrary[id].read = !myLibrary[id].read;
   listBooks();
 }
-$(document).ready(function () {
-  listBooks();
-});
