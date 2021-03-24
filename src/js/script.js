7f80d293169e05ec0d1e4e7ccd6eac4a98fb2459
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-24 15:04:34 -0600

    Fix linters error 2

--- src/js/script.js
+++ src/js/script.js
@@ -7,14 +7,12 @@ function Book(name, author, numpages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  const name = document.getElementById('name').value;
-  const author = document.getElementById('author').value;
-  const numpages = document.getElementById('numPages').value;
-  const book = new Book(name, author, numpages, false);
-  myLibrary.push(book);
-  listBooks();
-  emptyInputs();
+function listBooks() {
+  document.getElementById('list').innerHTML = '';
+  const d = document.getElementById('list');
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    d.innerHTML += '<div class="col-5"><div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + myLibrary[i].name + ' (' + myLibrary[i].numpages + ' pages)</h5><h6 class="card-subtitle mb-2 text-muted">' + myLibrary[i].author + '</h6><p class="card-text">Have you read it before? ' + myLibrary[i].read + '</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook(' + i + ')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook(' + i + ')" class="card-link">Change read</button></div></div></div></div>';
+  }
 }
 
 function emptyInputs() {
@@ -23,12 +21,14 @@ function emptyInputs() {
   document.getElementById('numPages').value = '';
 }
 
-function listBooks() {
-  document.getElementById('list').innerHTML = '';
-  d = document.getElementById('list');
-  for (let i = 0; i < myLibrary.length; i++) {
-    d.innerHTML += '<div class="col-5"><div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + myLibrary[i].name + ' (' + myLibrary[i].numpages + ' pages)</h5><h6 class="card-subtitle mb-2 text-muted">' + myLibrary[i].author + '</h6><p class="card-text">Have you read it before? ' + myLibrary[i].read + '</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook(' + i + ')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook(' + i + ')" class="card-link">Change read</button></div></div></div></div>';
-  }
+function addBookToLibrary() {
+  const name = document.getElementById('name').value;
+  const author = document.getElementById('author').value;
+  const numpages = document.getElementById('numPages').value;
+  const book = new Book(name, author, numpages, false);
+  myLibrary.push(book);
+  listBooks();
+  emptyInputs();
 }
 
 function deleteBook(id) {
