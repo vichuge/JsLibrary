4ba929eef661814e0d0a47144d13325c14ea4ffe
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-24 12:11:19 -0600

    Fix linters issues

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,54 @@
-let myLibrary = []
+const myLibrary = [];
 
 function Book(name, author, numpages, read) {
-    this.name = name;
-    this.author = author;
-    this.numpages = numpages;
-    this.read = read;
+  this.name = name;
+  this.author = author;
+  this.numpages = numpages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-    let name = $('#name').val()
-    let author = $('#author').val()
-    let numpages = $('#numPages').val()
-    const book = new Book(name, author, numpages, false)
-    myLibrary.push(book)
-    listBooks()
-    emptyInputs()
-    console.log(myLibrary)
+  const name = $('#name').val()
+  const author = $('#author').val()
+  const numpages = $('#numPages').val()
+  const book = new Book(name, author, numpages, false)
+  myLibrary.push(book)
+  listBooks()
+  emptyInputs()
+  console.log(myLibrary)
 }
 
 function emptyInputs() {
-    $('#name').val('')
-    $('#author').val('')
-    $('#numPages').val('')
+  $('#name').val('')
+  $('#author').val('')
+  $('#numPages').val('')
 }
 
 function listBooks() {
-    $('#list').empty()
-    for (i = 0; i < myLibrary.length; i++) {
-        $('#list').append(
-            d = $('<div>').prop({
-                //innerHTML: 'Name: ' + myLibrary[i].name + '\r\n' +
-                //    'Author: ' + myLibrary[i].author,
-                className: 'col-4'
-            }),
-            d.append('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">'+myLibrary[i].name+' ('+myLibrary[i].numpages+' pages)</h5><h6 class="card-subtitle mb-2 text-muted">'+myLibrary[i].author+'</h6><p class="card-text">Have you read it before? '+myLibrary[i].read+'</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook('+i+')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook('+i+')" class="card-link">Change read</button></div></div></div>')
-        );
-        
-        //console.log(myLibrary[i])
-    }
+  $('#list').empty()
+  for (i = 0; i < myLibrary.length; i++) {
+    $('#list').append(
+      d = $('<div>').prop({
+        //innerHTML: 'Name: ' + myLibrary[i].name + '\r\n' +
+        //  'Author: ' + myLibrary[i].author,
+        className: 'col-4'
+      }),
+      d.append('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">'+myLibrary[i].name+' ('+myLibrary[i].numpages+' pages)</h5><h6 class="card-subtitle mb-2 text-muted">'+myLibrary[i].author+'</h6><p class="card-text">Have you read it before? '+myLibrary[i].read+'</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook('+i+')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook('+i+')" class="card-link">Change read</button></div></div></div>')
+    );
+    
+    //console.log(myLibrary[i])
+  }
 }
 
 function deleteBook(id) {
-    myLibrary.splice(id, 1)
-    listBooks()
+  myLibrary.splice(id, 1)
+  listBooks()
 }
 
 function readBook(id) {
-    myLibrary[id].read = !myLibrary[id].read
-    listBooks()
+  myLibrary[id].read = !myLibrary[id].read
+  listBooks()
 }
 $(document).ready(function () {
-    listBooks()
+  listBooks()
 });
