e7fe5665fe04c3f21d4acedd009629af6c26b589
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-24 15:13:41 -0600

    Fix linters error 3

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,20 @@ function listBooks() {
   document.getElementById('list').innerHTML = '';
   const d = document.getElementById('list');
   for (let i = 0; i < myLibrary.length; i += 1) {
-    d.innerHTML += '<div class="col-5"><div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' + myLibrary[i].name + ' (' + myLibrary[i].numpages + ' pages)</h5><h6 class="card-subtitle mb-2 text-muted">' + myLibrary[i].author + '</h6><p class="card-text">Have you read it before? ' + myLibrary[i].read + '</p><div class="d-grid gap-2"><button class="btn btn-primary" onClick="deleteBook(' + i + ')" class="card-link">Delete</button><button class="btn btn-primary" onClick="readBook(' + i + ')" class="card-link">Change read</button></div></div></div></div>';
+    d.innerHTML += `
+    <div class="col-5">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${myLibrary[i].name} ( ${myLibrary[i].numpages} pages)</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${myLibrary[i].author}</h6>
+          <p class="card-text">Have you read it before? ${myLibrary[i].read}</p>
+          <div class="d-grid gap-2">
+            <button class="btn btn-primary" onClick="deleteBook(${i})" class="card-link">Delete</button>
+            <button class="btn btn-primary" onClick="readBook(${i})" class="card-link">Change read</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
   }
 }
 
