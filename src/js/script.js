0272fbe1825e7e4b9f434f4253530d108e7e4e3c
V <victor.hugo.pacheco.flores@gmail.com>
2021-03-26 11:18:31 -0600

    Fix linters errors

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 const myLibrary = [];
 
-/*function Book(name, author, numpages, read) {
+/*  function Book(name, author, numpages, read) {
   this.name = name;
   this.author = author;
   this.numpages = numpages;
   this.read = read;
-}*/
+} */
 
 const Book = (name, author, numpages, read) => {
   return { name, author, numpages, read };
@@ -67,7 +67,7 @@ function addBookToLibrary() {
     return;
   }
 
-  //const book = new Book(name, author, numpages, read.checked);
+  //  const book = new Book(name, author, numpages, read.checked);
   const book = Book(name, author, numpages, read.checked);
   myLibrary.push(book);
   listBooks();
